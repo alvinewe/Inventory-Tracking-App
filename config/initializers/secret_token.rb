3fc4d964d7659258399015546adc22a8c6c039b7
Sheng-Loong SU <sushengloong@gmail.com>
2011-08-28 23:10:08 +0800

    Restarted project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventoryTrackingApp::Application.config.secret_token = 'd3d8a41a1582c72c69bcde5c431b9a95cf1de72116cf7f843d509cc2feac1ce965b37a9359fda0d0975641c216ad54d3d9f31b0703cba3019fece2abc3a67151'
