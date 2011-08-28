6cbd71308681e6e6f17cc1507483e8b22240c133
Sheng-Loong SU <sushengloong@gmail.com>
2011-08-28 22:56:32 +0800

    cleared all stuff

--- app/models/inventory.rb
+++
@@ -1,6 +0,0 @@
-class Inventory < ActiveRecord::Base
-  validates :name, :quantity, :cost_price, :presence => true
-  validates :name, :uniqueness => true
-  validates :quantity, :cost_price, 
-    :numericality => {:greater_than_or_equal_to => 0}
-end
